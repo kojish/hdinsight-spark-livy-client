00e1fb8e341b66c79a4eeb167521c4f7ef1d4755
Koji Shimizu <koji.shimizu@outlook.com>
2017-08-04 09:26:11 +0900

    Add jackson support

---
+++ src/main/java/delsh/livy/BatchJobRequest.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package delsh.livy;
+
+/**
+ * POJO class reqresenting a request for a new batch job.
+ */
+public class BatchJobRequest {
+
+	public String proxyUser;
+	public String file;
+	public String[] args;
+	public String className;
+	public String[] jars;
+	public String[] pyFiles;
+	public String[] files;
+	public String driverMemory;
+	public int driverCores;
+	public String executorMemory;
+	public int executorCores;
+	public int numExecutors;
+	public String[] archives;
+
+	@Override
+	public String toString() {
+		return "";
+	}
+}
